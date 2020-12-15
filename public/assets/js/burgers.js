8e729664140247a5441a373b1019bc9b828f4bd5
Mostafa <m.alsham627@gmail.com>
2020-12-14 22:11:05 -0700

    assets

---
+++ public/assets/js/burgers.js
@@ -0,0 +1,38 @@
+// Make sure we wait to attach our handlers until the DOM is fully loaded.
+$(function () {
+    
+    $(".change-state").on("click", function (event) {
+
+      const id = $(this).data("id");
+  
+      // Send the PUT request.
+      $.ajax("/api/burgers/" + id, {
+        type: "PUT"
+      }).then(function () {
+        console.log("Yumm!");
+
+        // Reload the page to get the updated list
+        location.reload();
+      });
+    });
+  
+    $(".create-form").on("submit", function (event) {
+      // Make sure to preventDefault on a submit event.
+      event.preventDefault();
+  
+      const newBurger = {
+        name: $("#burger").val().trim()
+      };
+  
+      // Send the POST request.
+      $.ajax("/api/cats", {
+        type: "POST",
+        data: newBurger,
+      }).then(function () {
+        console.log("New burger ready to be devoured!");
+        // Reload the page to get the updated list
+        location.reload();
+      });
+    });
+  });
+  
