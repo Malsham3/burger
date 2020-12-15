646959bbf50ac064756f76954c1adc27db829b04
Mostafa <m.alsham627@gmail.com>
2020-12-14 19:19:40 -0700

    seeds

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+USE burgers_db;
+
+INSERT INTO burgers (burger_name) VALUES ('Double-Double');
+INSERT INTO burgers (burger_name) VALUES ('Cheese Burger');
+INSERT INTO burgers (burger_name) VALUES ('Animal Style 3x3');
